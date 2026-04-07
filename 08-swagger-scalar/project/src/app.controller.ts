8000ac357cf021fe9ce69454a0f0d64da4473c97
MdHasibAskari <mdhsbaskari@gmail.com>
2026-04-07 15:07:41 +0600

    feat: add Swagger integration and API reference with new endpoints in AppController

--- 08-swagger-scalar/project/src/app.controller.ts
+++ 08-swagger-scalar/project/src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('App Controller')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiTags('Hello Endpoint')
+  @ApiOperation({ summary: 'Get a greeting message' })
+  @ApiResponse({ status: 200, description: 'Successful response', type: String })
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('number')
+  @ApiOperation({ summary: 'Get a number' })
+  @ApiResponse({ status: 200, description: 'Successful response', type: Number })
+  getNumber(): number {
+    return 42;
+  }
+
+  @Get('get-product/:id')
+  @ApiParam({ name: 'id', description: 'Product ID', type: String })
+  @ApiOperation({ summary: 'Get a product' })
+  @ApiResponse({ status: 200, description: 'Successful response', type: Object })
+  getProduct(@Param('id') id: string): object {
+    return { id, name: 'Product 1', price: 100 };
   }
 }
--- 08-swagger-scalar/project/src/main.ts
+++ 08-swagger-scalar/project/src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Swagger Example')
+    .setDescription('API documentation for Swagger example')
+    .setVersion('1.0')
+    .addServer('http://localhost:5000', 'Local server')
+    .addServer('http://localhost:3000', 'Local server')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    jsonDocumentUrl: '/api-json',
+  });
+
+  app.use('/docs', apiReference({
+    title: 'API Reference',
+    // content: documentFactory(),
+    url: '/api-json',
+    theme: 'saturn',
+    layout: 'modern',
+    defaultHttpClient: {
+      targetKey: 'js',
+      clientKey: 'fetch'
+    },
+    metaData: {
+      version: '1.0',
+      description: 'API documentation for Swagger example',
+    },
+  }))
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
