b4278f9805c4e06074d99ac3d3712734ec8abbfa
MdHasibAskari <mdhsbaskari@gmail.com>
2026-04-07 23:12:19 +0600

    feat: implement user creation and retrieval with Mongoose integration

--- 02-mongodb/project/src/user/dto/create-user.dto.ts
+++ 02-mongodb/project/src/user/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger"
+
+export class CreateUserDto {
+    @ApiProperty({ name: 'email', example: 'john.doe@example.com' })
+    email: string
+
+    @ApiProperty({ name: 'role', example: 'user' })
+    role: string
+}
--- 02-mongodb/project/src/user/user.service.ts
+++ 02-mongodb/project/src/user/user.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './users.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
+
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>
+  ) { }
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.userModel.findById(id).exec();
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.userModel.findByIdAndDelete(id).exec();
   }
 }
