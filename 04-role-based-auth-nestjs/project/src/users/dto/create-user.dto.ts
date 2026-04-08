fb3068105d9a711ffe22406670225948a3611d4b
MdHasibAskari <mdhsbaskari@gmail.com>
2026-04-08 22:31:38 +0600

    feat(users): implement user creation, retrieval, and role management with Mongoose integration

--- 04-role-based-auth-nestjs/project/src/users/dto/create-user.dto.ts
+++ 04-role-based-auth-nestjs/project/src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsEnum, Min } from "class-validator";
+import { Role } from "../../auth/enums/roles.enum";
+
+export class CreateUserDto {
+    @ApiProperty({ example: 'john.doe@example.com' })
+    @IsEmail()
+    email!: string;
+
+    @ApiProperty({ example: 'password123' })
+    @Min(6)
+    password!: string;
+
+    @ApiProperty({ example: ['user'] })
+    @IsEnum(Role, { each: true })
+    roles?: string[];
+}
---
+++ 04-role-based-auth-nestjs/project/src/users/users.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+import { Role } from "../auth/enums/roles.enum";
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+    @Prop({ required: true })
+    email!: string;
+
+    @Prop({ required: true, min: 6 })
+    password!: string;
+
+    @Prop({ required: true, type: [String], default: [Role.User], enum: Role })
+    roles!: string[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
