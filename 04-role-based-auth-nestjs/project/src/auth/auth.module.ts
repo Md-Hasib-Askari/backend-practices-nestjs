7c6bb85937157ac72b5f14a1b4a987ab49191a2d
MdHasibAskari <mdhsbaskari@gmail.com>
2026-04-08 22:31:55 +0600

    feat(auth): implement JWT authentication with user validation and role management

--- 04-role-based-auth-nestjs/project/src/auth/auth.module.ts
+++ 04-role-based-auth-nestjs/project/src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: 'your_jwt_secret',
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+    UsersModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService],
 })
 export class AuthModule {}
--- 04-role-based-auth-nestjs/project/src/auth/auth.service.ts
+++ 04-role-based-auth-nestjs/project/src/auth/auth.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) { }
 
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
 }
