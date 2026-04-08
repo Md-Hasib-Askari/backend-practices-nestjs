fb3e8ca88e3c1a651182fb3f34f3324e33a8b376
MdHasibAskari <mdhsbaskari@gmail.com>
2026-04-08 21:22:59 +0600

    feat: add Mongoose integration and update module imports for database connection

--- 04-role-based-auth-nestjs/project/src/app.module.ts
+++ 04-role-based-auth-nestjs/project/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: 'mongodb://root:root@localhost:27017/nestjs-practice?authSource=admin',
+      }),
+    }),
+    AuthModule, UsersModule, PostsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
