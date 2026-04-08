80eb8af19ea545890b7418ee2d226ddb66e52617
MdHasibAskari <mdhsbaskari@gmail.com>
2026-04-08 22:31:47 +0600

    feat: add Swagger documentation and cookie parser middleware to main application

--- 04-role-based-auth-nestjs/project/src/main.ts
+++ 04-role-based-auth-nestjs/project/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Role-Based Authentication Example')
+    .setDescription('API documentation for the NestJS Role-Based Authentication example')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addTag('users')
+    .addTag('posts')
+    .build();
+  const document = await SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
